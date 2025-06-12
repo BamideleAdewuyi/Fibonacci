0fec18d155ff24e56767f34b0868a5f2eb99dbab
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-12 13:43:12 +0100

    index.js: Wrote fibs function

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function fibs(num) {
+    if (num === 1) {
+        return [0];
+    }
+    let result = [0, 1];
+    for (i = 2; i < num; i++) {
+        result.push(result[i-2]+ result[i-1])
+    }
+
+    return result;
+
+}
+
+console.log(fibs(3))
