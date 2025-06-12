2f7a445ba49b74276c17cf24ce38e5220e925109
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-06-12 16:05:06 +0100

    index.js: Wrote fibsRec

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-function fibs(num) {
-    if (num === 1) {
+function fibs(n) {
+    if (n === 1) {
         return [0];
     }
     let result = [0, 1];
-    for (i = 2; i < num; i++) {
+    for (i = 2; i < n; i++) {
         result.push(result[i-2]+ result[i-1])
     }
 
@@ -11,4 +11,17 @@ function fibs(num) {
 
 }
 
-console.log(fibs(3))
+function fibsRec(n) {
+
+    if (n == 0) return [];
+    if (n == 1) return [0];
+    if (n == 2) return [0, 1];
+    
+    let result = fibsRec(n - 1);
+    result.push(result[result.length - 1] + result[result.length - 2])
+
+
+    return result
+}
+
+console.log(fibsRec(8))
